0611c603d76c9ad219dc9c07117c796139688a4b
IuriiSh <genoffsg@gmail.com>
2023-04-23 15:35:57 +0200

    1.26c - багфикс
    Общие изменения:
    1. Курак специализация изменена: После каждого применения клича на случайное свое существо герой применяет Зов крови (Исключая виверн). Мана на этот клич не требуется. Клич не срабатывает два раза подряд на один и тот же отряд.  (было: могло сработать два раза подряд на один и тот же отряд)
    
    2. В диалоговых окнах реррола рун и заклинаний дополнено описание (во избежание ошибок игроков)
    
    Исправленные баги:
    3. Покровительство Асхи: шанс срабатывания на Покровительство Асхи был на 10% ниже на всех уровнях удачи (на 1 удачи шанс срабатывания был равен 0).
    4. Покровительство Асхи: теперь вероятность не может превышать 50%. Ранее вероятность могла превышать 50%. При 11 удачи ваши заклинания не тратили ману.
    5. Сад славного народца: исправлен баг, когда за перегенерацию основных характеристик не снимались деньги.
    6. Навигация: исправлена ситуация, когда игрок мог взять и использовать артефакт на 4 или 5 день.
    
    7. Курак: герой больше не запрещает творить заклинания)
    8. Курак: исправлен баг, когда Восполнение маны не срабатывала после использования кличей.
    
    9. Корона лидерства перенесена в мажоры.
    10. Том силы перенесен в реликты.
    11. Посох Сар-Исса перенесен в мажоры.
    
    12. Исправлены описания навыка казны, специализации Илаи и...другие...
    
    1.26d
    
    Общие изменения:
    1, Курак специализация изменена: После каждого применения клича случайное существо получает 225 очков крови. Эффект не срабатывает два раза подряд на один и тот же отряд.
    2. Перегенерация рун стала дешевле: 2/2/2/3/4 тысяч золота (было: 3/3/3/4/5).
    3. Перегенерация основных характеристик в саде славного народца теперь стоит 3000 золота (было: 2000).
    
    Исправленные баги:
    Метка огня действует 3 хода (было: 2 хода)
    Артефакты, которые были перенесены в реликт в патче 1.26a больше не могут быть получены с помощью навыка Трофеи.
    Игра больше не зависает при перегенерации заклинаний, если у вас был бонус заклинаний.
    Цены при перегенерации рун теперь корректны.
    Острый ум. Исправлено описание - В начале сражения герой получает бонус равный 0,25 к стартовой позиции на ATB-шкале. (было: 0,3).
    
    1.26e - (исправление критических багов)
    
    Исправление багов:
    1. Перегенерация заклинаний для 3 уровня теперь работает корректно.

---
+++ Half-RTA/Maps/Multiplayer/Half-RTA_1.26e/day1/choice_of_races/common.lua
@@ -0,0 +1,205 @@
+-- Файл, описывающий общий функционал между черками рас
+
+-- Выключение взаимодействия игроков с окружением
+function disableAreaInteractive()
+  print "disableAreaInteractive"
+
+  SetObjectEnabled('red1', nil);  SetDisabledObjectMode('red1', DISABLED_BLOCKED);
+  SetObjectEnabled('red2', nil);  SetDisabledObjectMode('red2', DISABLED_BLOCKED);
+  SetObjectEnabled('red3', nil);  SetDisabledObjectMode('red3', DISABLED_BLOCKED);
+  SetObjectEnabled('red4', nil);  SetDisabledObjectMode('red4', DISABLED_BLOCKED);
+  SetObjectEnabled('red5', nil);  SetDisabledObjectMode('red5', DISABLED_BLOCKED);
+  SetObjectEnabled('red6', nil);  SetDisabledObjectMode('red6', DISABLED_BLOCKED);
+  SetObjectEnabled('red7', nil);  SetDisabledObjectMode('red7', DISABLED_BLOCKED);
+  SetObjectEnabled('red8', nil);  SetDisabledObjectMode('red8', DISABLED_BLOCKED);
+  SetObjectEnabled('red9', nil);  SetDisabledObjectMode('red9', DISABLED_BLOCKED);
+  SetObjectEnabled('red10', nil); SetDisabledObjectMode('red10', DISABLED_BLOCKED);
+  SetObjectEnabled('red12', nil); SetDisabledObjectMode('red12', DISABLED_BLOCKED);
+  SetObjectEnabled('red13', nil); SetDisabledObjectMode('red13', DISABLED_BLOCKED);
+  SetObjectEnabled('red14', nil); SetDisabledObjectMode('red14', DISABLED_BLOCKED);
+  SetObjectEnabled('red15', nil); SetDisabledObjectMode('red15', DISABLED_BLOCKED);
+  SetObjectEnabled('red16', nil); SetDisabledObjectMode('red16', DISABLED_BLOCKED);
+  SetObjectEnabled('red17', nil); SetDisabledObjectMode('red17', DISABLED_BLOCKED);
+  SetObjectEnabled('red18', nil); SetDisabledObjectMode('red18', DISABLED_BLOCKED);
+  SetObjectEnabled('red19', nil); SetDisabledObjectMode('red19', DISABLED_BLOCKED);
+  SetObjectEnabled('red20', nil); SetDisabledObjectMode('red20', DISABLED_BLOCKED);
+
+  SetObjectEnabled('blue1', nil);  SetDisabledObjectMode('blue1', DISABLED_BLOCKED);
+  SetObjectEnabled('blue2', nil);  SetDisabledObjectMode('blue2', DISABLED_BLOCKED);
+  SetObjectEnabled('blue3', nil);  SetDisabledObjectMode('blue3', DISABLED_BLOCKED);
+  SetObjectEnabled('blue4', nil);  SetDisabledObjectMode('blue4', DISABLED_BLOCKED);
+  SetObjectEnabled('blue5', nil);  SetDisabledObjectMode('blue5', DISABLED_BLOCKED);
+  SetObjectEnabled('blue6', nil);  SetDisabledObjectMode('blue6', DISABLED_BLOCKED);
+  SetObjectEnabled('blue7', nil);  SetDisabledObjectMode('blue7', DISABLED_BLOCKED);
+  SetObjectEnabled('blue8', nil);  SetDisabledObjectMode('blue8', DISABLED_BLOCKED);
+  SetObjectEnabled('blue9', nil);  SetDisabledObjectMode('blue9', DISABLED_BLOCKED);
+  SetObjectEnabled('blue10', nil); SetDisabledObjectMode('blue10', DISABLED_BLOCKED);
+  SetObjectEnabled('blue11', nil); SetDisabledObjectMode('blue11', DISABLED_BLOCKED);
+  SetObjectEnabled('blue12', nil); SetDisabledObjectMode('blue12', DISABLED_BLOCKED);
+  SetObjectEnabled('blue13', nil); SetDisabledObjectMode('blue13', DISABLED_BLOCKED);
+  SetObjectEnabled('blue14', nil); SetDisabledObjectMode('blue14', DISABLED_BLOCKED);
+  SetObjectEnabled('blue15', nil); SetDisabledObjectMode('blue15', DISABLED_BLOCKED);
+  SetObjectEnabled('blue16', nil); SetDisabledObjectMode('blue16', DISABLED_BLOCKED);
+  SetObjectEnabled('blue17', nil); SetDisabledObjectMode('blue17', DISABLED_BLOCKED);
+  SetObjectEnabled('blue18', nil); SetDisabledObjectMode('blue18', DISABLED_BLOCKED);
+  SetObjectEnabled('blue19', nil); SetDisabledObjectMode('blue19', DISABLED_BLOCKED);
+  SetObjectEnabled('blue20', nil); SetDisabledObjectMode('blue20', DISABLED_BLOCKED);
+
+  SetObjectEnabled('human1', nil); SetDisabledObjectMode('human1', DISABLED_BLOCKED);
+  SetObjectEnabled('human2', nil); SetDisabledObjectMode('human2', DISABLED_BLOCKED);
+  SetObjectEnabled('demon1', nil); SetDisabledObjectMode('demon1', DISABLED_BLOCKED);
+  SetObjectEnabled('demon2', nil); SetDisabledObjectMode('demon2', DISABLED_BLOCKED);
+  SetObjectEnabled('nekr1', nil);  SetDisabledObjectMode('nekr1', DISABLED_BLOCKED);
+  SetObjectEnabled('nekr2', nil);  SetDisabledObjectMode('nekr2', DISABLED_BLOCKED);
+  SetObjectEnabled('elf1', nil);   SetDisabledObjectMode('elf1', DISABLED_BLOCKED);
+  SetObjectEnabled('elf2', nil);   SetDisabledObjectMode('elf2', DISABLED_BLOCKED);
+  SetObjectEnabled('mag1', nil);   SetDisabledObjectMode('mag1', DISABLED_BLOCKED);
+  SetObjectEnabled('mag2', nil);   SetDisabledObjectMode('mag2', DISABLED_BLOCKED);
+  SetObjectEnabled('liga1', nil);  SetDisabledObjectMode('liga1', DISABLED_BLOCKED);
+  SetObjectEnabled('liga2', nil);  SetDisabledObjectMode('liga2', DISABLED_BLOCKED);
+  SetObjectEnabled('gnom1', nil);  SetDisabledObjectMode('gnom1', DISABLED_BLOCKED);
+  SetObjectEnabled('gnom2', nil);  SetDisabledObjectMode('gnom2', DISABLED_BLOCKED);
+  SetObjectEnabled('ork1', nil);   SetDisabledObjectMode('ork1', DISABLED_BLOCKED);
+  SetObjectEnabled('ork2', nil);   SetDisabledObjectMode('ork2', DISABLED_BLOCKED);
+
+  SetObjectEnabled('human1vrag', nil); SetDisabledObjectMode('human1vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('human2vrag', nil); SetDisabledObjectMode('human2vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('demon1vrag', nil); SetDisabledObjectMode('demon1vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('demon2vrag', nil); SetDisabledObjectMode('demon2vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('nekr1vrag', nil);  SetDisabledObjectMode('nekr1vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('nekr2vrag', nil);  SetDisabledObjectMode('nekr2vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('elf1vrag', nil);   SetDisabledObjectMode('elf1vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('elf2vrag', nil);   SetDisabledObjectMode('elf2vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('mag1vrag', nil);   SetDisabledObjectMode('mag1vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('mag2vrag', nil);   SetDisabledObjectMode('mag2vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('liga1vrag', nil);  SetDisabledObjectMode('liga1vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('liga2vrag', nil);  SetDisabledObjectMode('liga2vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('gnom1vrag', nil);  SetDisabledObjectMode('gnom1vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('gnom2vrag', nil);  SetDisabledObjectMode('gnom2vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('ork1vrag', nil);   SetDisabledObjectMode('ork1vrag', DISABLED_BLOCKED);
+  SetObjectEnabled('ork2vrag', nil);   SetDisabledObjectMode('ork2vrag', DISABLED_BLOCKED);
+end;
+
+-- Удаление всех перегородок
+function deleteAllDelimeters()
+  print "deleteAllDelimeters"
+
+  RemoveObject     ('red1');
+  RemoveObject     ('red2');
+  RemoveObject     ('red3');
+  RemoveObject     ('red4');
+  RemoveObject     ('red5');
+  RemoveObject     ('red6');
+  RemoveObject     ('red7');
+  RemoveObject     ('red8');
+  RemoveObject     ('red9');
+  RemoveObject     ('red12');
+  RemoveObject     ('red13');
+  RemoveObject     ('red14');
+  RemoveObject     ('red15');
+  RemoveObject     ('red16');
+  RemoveObject     ('red17');
+  RemoveObject     ('red18');
+  RemoveObject     ('red19');
+  RemoveObject     ('red20');
+
+  RemoveObject     ('blue1');
+  RemoveObject     ('blue2');
+  RemoveObject     ('blue3');
+  RemoveObject     ('blue4');
+  RemoveObject     ('blue5');
+  RemoveObject     ('blue6');
+  RemoveObject     ('blue7');
+  RemoveObject     ('blue8');
+  RemoveObject     ('blue9');
+  RemoveObject     ('blue12');
+  RemoveObject     ('blue13');
+  RemoveObject     ('blue14');
+  RemoveObject     ('blue15');
+  RemoveObject     ('blue16');
+  RemoveObject     ('blue17');
+  RemoveObject     ('blue18');
+  RemoveObject     ('blue19');
+  RemoveObject     ('blue20');
+end;
+
+--  всех перегородок
+function moveDelimetersToRandomChoise()
+  print "moveDelimetersToRandomChoise"
+  
+  SetObjectPosition('red12', 44, 24);
+  SetObjectPosition('red13', 45, 24);
+  SetObjectPosition('red14', 46, 24);
+  SetObjectPosition('red15', 44, 22);
+  SetObjectPosition('red18', 44, 20);
+  SetObjectPosition('red19', 45, 20);
+  SetObjectPosition('red20', 46, 20);
+  SetObjectPosition('blue1', 37, 89);
+  SetObjectPosition('blue2', 38, 89);
+  SetObjectPosition('blue3', 39, 89);
+  SetObjectPosition('blue4', 37, 87);
+  SetObjectPosition('blue7', 37, 85);
+  SetObjectPosition('blue8', 38, 85);
+  SetObjectPosition('blue9', 39, 85);
+  SetObjectPosition('red4', 33, 88)
+  SetObjectPosition('red5', 33, 86)
+  SetObjectPosition('red16', 44, 23)
+  SetObjectPosition('red17', 44, 21)
+  
+  SetObjectPosition('blue5', 37, 86)
+  SetObjectPosition('blue6', 37, 88)
+  SetObjectPosition('blue15', 40, 23)
+  SetObjectPosition('blue16', 40, 21)
+end;
+
+-- соотношение расы с отображаемыми при черке существами
+MAPPING_RACE_TO_CREATURES = {
+  -- Орден Порядка - латник и ревнитель веры
+  [RACES.HAVEN] = { ID1 = CREATURE_FOOTMAN, ID2 = CREATURE_VINDICATOR },
+  -- Инферно - черт и дьяволенок
+  [RACES.INFERNO] = { ID1 = CREATURE_IMP, ID2 = CREATURE_QUASIT },
+  -- Некрополис - вампир и князья вампиров
+  [RACES.NECROPOLIS] = { ID1 = CREATURE_VAMPIRE, ID2 = CREATURE_NOSFERATU },
+  -- Лесной Союз - лучник и стрелок
+  [RACES.SYLVAN] = { ID1 = CREATURE_WOOD_ELF, ID2 = CREATURE_SHARP_SHOOTER },
+  -- Акадения Волшебства - маг и боевой маг
+  [RACES.ACADEMY] = { ID1 = CREATURE_MAGI, ID2 = CREATURE_COMBAT_MAGE },
+  -- Лига Теней - бестия и фурия
+  [RACES.DUNGEON] = { ID1 = CREATURE_WITCH, ID2 = CREATURE_BLOOD_WITCH_2 },
+  -- Дварфы - жрец рун и служитель огня
+  [RACES.FORTRESS] = { ID1 = CREATURE_RUNE_MAGE, ID2 = CREATURE_FLAME_KEEPER },
+  -- Орда - палач и вожак
+  [RACES.STRONGHOLD] = { ID1 = CREATURE_ORCCHIEF_BUTCHER, ID2 = CREATURE_ORCCHIEF_CHIEFTAIN },
+};
+
+-- Получение ключа случайной расы
+function getRandomRace()
+  print "getRandomRace"
+
+  return random(8);
+end;
+
+-- Список всех фракций с юнитами для обоих игроков и координатами их начальной постановки для простого черка
+ALL_RACES_WITH_COORDINATES = {
+  { raceId = RACES.HAVEN,       question = "question_play_haven.txt",          [PLAYER_1] = { unit = 'human1', x = 31, y = 88 }, [PLAYER_2] = { unit = 'human2vrag', x = 46, y = 23 } },
+  { raceId = RACES.INFERNO,     question = "question_play_inferno.txt",        [PLAYER_1] = { unit = 'demon1', x = 31, y = 86 }, [PLAYER_2] = { unit = 'demon2vrag', x = 46, y = 21 } },
+  { raceId = RACES.NECROPOLIS,  question = "question_play_necropolis.txt",     [PLAYER_1] = { unit = 'nekr1',  x = 31, y = 90 }, [PLAYER_2] = { unit = 'nekr2vrag',  x = 46, y = 25 } },
+  { raceId = RACES.SYLVAN,      question = "question_play_sylvan.txt",         [PLAYER_1] = { unit = 'elf1',   x = 31, y = 84 }, [PLAYER_2] = { unit = 'elf2vrag',   x = 46, y = 19 } },
+  { raceId = RACES.ACADEMY,     question = "question_play_academy.txt",        [PLAYER_1] = { unit = 'mag1',   x = 38, y = 23 }, [PLAYER_2] = { unit = 'mag2vrag',   x = 39, y = 88 } },
+  { raceId = RACES.DUNGEON,     question = "question_play_dungeon.txt",        [PLAYER_1] = { unit = 'liga1',  x = 38, y = 21 }, [PLAYER_2] = { unit = 'liga2vrag',  x = 39, y = 86 } },
+  { raceId = RACES.FORTRESS,    question = "question_play_fortress.txt",       [PLAYER_1] = { unit = 'gnom1',  x = 38, y = 25 }, [PLAYER_2] = { unit = 'gnom2vrag',  x = 39, y = 90 } },
+  { raceId = RACES.STRONGHOLD,  question = "question_play_stronghold.txt",     [PLAYER_1] = { unit = 'ork1',   x = 38, y = 19 }, [PLAYER_2] = { unit = 'ork2vrag',   x = 39, y = 84 } },
+};
+
+-- Удаление всех юнитов, для выбора рас
+function deleteAllRacesUnit()
+  print "deleteAllRacesUnit"
+
+  for indexRace = 1, length(ALL_RACES_WITH_COORDINATES) do
+    local currentRace = ALL_RACES_WITH_COORDINATES[indexRace];
+
+    SetObjectPosition(currentRace[PLAYER_1].unit, 1, 1, UNDERGROUND)
+    SetObjectPosition(currentRace[PLAYER_2].unit, 1, 1, UNDERGROUND)
+  end;
+end;
+  
